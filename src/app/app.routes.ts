5917fe17c344acf19df0076437e03796af5736cd
federico-arevalo <arevalofd98@gmail.com>
2024-06-04 18:24:33 -0300

    add validations to form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'landing', component: LandingComponent },
   // {
   //     path: 'quiensoy',
   //     loadComponent: () =>
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -5,6 +5,7 @@ import {
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
@@ -23,11 +24,31 @@ import { AuthService } from '../../services/auth/auth.service';
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  registerForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirmPass: new FormControl(''),
+  pacienteForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    age: new FormControl('', [Validators.required, Validators.min(18)]),
+    dni: new FormControl('', [Validators.required, Validators.maxLength(6)]),
+    obraSocial: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    repeatPassword: new FormControl('', [Validators.required]),
+    firstProfilePicture: new FormControl('', [Validators.required]),
+    secondProfilePicture: new FormControl('', [Validators.required]),
   });
+
+  especialistaForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    age: new FormControl('', [Validators.required, Validators.min(18)]),
+    dni: new FormControl('', [Validators.required, Validators.maxLength(6)]),
+    especialidad: new FormControl([''], Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    repeatPassword: new FormControl('', [Validators.required]),
+    profilePicture: new FormControl('', [Validators.required]),
+  });
+
   isPaciente = true;
 
   constructor(private authService: AuthService) {}
@@ -36,9 +57,10 @@ export class RegisterComponent {
     $event.preventDefault();
 
     if (
-      this.registerForm.value.password === this.registerForm.value.confirmPass
+      this.pacienteForm.value.password ===
+      this.pacienteForm.value.repeatPassword
     ) {
-      const { email, password } = this.registerForm.value;
+      const { email, password } = this.pacienteForm.value;
       this.authService.SignUp(email || '', password || '');
     } else {
       console.log('las contraseñas no coindicen');
