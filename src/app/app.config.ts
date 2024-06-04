15f7591911cb10712f3e54973675d886fecc75ed
federico-arevalo <arevalofd98@gmail.com>
2024-06-03 22:21:50 -0300

    add pages and components
    pages: login, register and notfound
    components: sidebar and footer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,26 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'federico-arevalo-clinica',
+        appId: '1:269601454300:web:e0da7befbcb72b17c35c19',
+        storageBucket: 'federico-arevalo-clinica.appspot.com',
+        apiKey: 'AIzaSyBjeqlFV2nWdUtMbxmRsgJlZRyOkCI3FbY',
+        authDomain: 'federico-arevalo-clinica.firebaseapp.com',
+        messagingSenderId: '269601454300',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  // {
+  //     path: 'quiensoy',
+  //     loadComponent: () =>
+  //       import('./pages/who-am-i/who-am-i.component').then(
+  //         (m) => m.WhoAmIComponent
+  //       ),
+  //   },
+  //   {
+  //     path: 'juegos',
+  //     children: [
+  //       {
+  //         path: '',
+  //         loadComponent: () =>
+  //           import('./pages/games/games.component').then((m) => m.GamesComponent),
+  //       },
+  //       {
+  //         path: 'mayormenor',
+  //         loadComponent: () =>
+  //           import('./components/games/mayor-menor/mayor-menor.component').then(
+  //             (m) => m.MayorMenorComponent
+  //           ),
+  //       },
+
+  //     ],
+  //     canActivate: [authGuard],
+  //   },
+  { path: '**', component: NotFoundComponent },
+];
