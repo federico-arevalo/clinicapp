99b309112b87be8433a69ee068c1f2fc3d78a738
federico-arevalo <arevalofd98@gmail.com>
2024-11-14 20:06:17 -0300

    add dropdown in mis turnos

--- src/app/pages/turnos/paciente/turnos-paciente/turnos-paciente.component.ts
+++ src/app/pages/turnos/paciente/turnos-paciente/turnos-paciente.component.ts
@@ -1,16 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-turnos-paciente',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './turnos-paciente.component.html',
   styleUrl: './turnos-paciente.component.scss',
 })
 export class TurnosPacienteComponent {
   showForm: boolean = false;
 
+  openDropdownIndex: number | null = null;
+
   openForm() {
     this.showForm = true;
+  }
+
+  cancelarTurno(id: string) {
+    console.log(id);
+    this.openDropdownIndex = null;
+  }
+
+  toggleDropdown(index: number) {
+    this.openDropdownIndex = this.openDropdownIndex === index ? null : index;
   }
 }
